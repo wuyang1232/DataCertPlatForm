21f11ec388e78fc2ff63419f5c6737cd1ae990c1
wuyang1232 <1403918572@qq.com>
2020-10-15 14:17:40 +0800

    获取用户上传的文件

--- controllers/uploadController.go
+++ controllers/uploadController.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"beego"
 	"fmt"
+	"os"
 	"strings"
 )
 
@@ -17,7 +18,9 @@ func (u *UploadController)Post(){
 	title := u.Ctx.Request.PostFormValue("index_title")//用户输入的标题
 
 	//用户上传的文件
-	file, header, err := u.GetFile("file")
+	file, header, err := u.GetFile("file")//封装好了。下面可以通过该名字获取文件
+	defer file.Close()
+
 	if err != nil{//解析客户端的文件出现错误
 		u.Ctx.WriteString("抱歉，文件解析失败，请重试！")
 		return
@@ -32,7 +35,7 @@ func (u *UploadController)Post(){
 		u.Ctx.WriteString("抱歉，文件类型不符合，请上传合适格式的文件")
 		return
 	}
-	//isJpg := strings.HasSuffix()
+	//isJpg := strings.HasSuffix(header.Filename,".jpg")
 
 	//文件大小200kb
 	config := beego.AppConfig
@@ -42,6 +45,53 @@ func (u *UploadController)Post(){
 		return
 	}
 	fmt.Println("文件的大小:",header.Size)//字节大小
+
+	//perm:permission权限
+	//权限的组成：a+b+c
+	//	a: 文件所有者对文件的操作权限： 读4，写2，执行1
+	//	b: 文件所有者所在组的用户操作权限： 读4，写2，执行1
+	//	c: 其他用户的操作权限： 读4，写2，执行1
+
+	//eg: m文件，权限651。什么意思？文件所有者6 = 4 + 2文件所有者有读写权限
+	// 文件所有者所在组的用户有写权限(错)
+	saveDir := "static/upload"
+	//打开文件
+	f, err := os.Open(saveDir)
+	if err != nil{
+		//创建文件夹
+		//err = os.Mkdir(saveDir,777)
+		//if err != nil{//打开文件夹失败，自己创建
+			//创建文件夹
+			err = os.Mkdir(saveDir,777)
+			if err != nil{
+				fmt.Println(err.Error())
+				u.Ctx.WriteString("抱歉，文件认证遇到错误，请重试！")
+				return
+			}
+
+			fmt.Println(err.Error())
+			u.Ctx.WriteString("抱歉，文件认证遇到错误，请重试！")
+			return
+		//}
+
+		fmt.Println(err.Error())
+		u.Ctx.WriteString("打开文件目录失败")
+		return
+		//打开目录遇到错误
+	}
+	fmt.Println("打开的文件夹：",f.Name())
+
+	//文件名：文件路径 + 文件名 + “.” + 文件拓展名
+	saveName :=saveDir +  "/" + header.Filename
+	fmt.Println("要保存的文件名",saveName)
+	//fromFile:文件
+	//toFile:要保存的文件路径
+	err = u.SaveToFile("file",saveName)//通过封装好的名字获取文件
+	if err != nil{
+		fmt.Println(err.Error())
+		u.Ctx.WriteString("文件认证失败，请重试")
+		return
+	}
 
 	fmt.Println("上传的文件:",file)
 	u.Ctx.WriteString("已获取到上传文件")
